f21f19ce25ff491b404e65330fd25a40db7fb1b9
BarutLutfi <102617579+BarutLutfi@users.noreply.github.com>
2024-11-18 10:42:44 +0300

    Initial Commit

---
+++ toDoUygulamasi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  toDoUygulamasi
+//
+//  Created by Lütfi Barut on 18.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ toDoUygulamasiTests/toDoUygulamasiTests.swift
@@ -0,0 +1,17 @@
+//
+//  toDoUygulamasiTests.swift
+//  toDoUygulamasiTests
+//
+//  Created by Lütfi Barut on 18.11.2024.
+//
+
+import Testing
+@testable import toDoUygulamasi
+
+struct toDoUygulamasiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
